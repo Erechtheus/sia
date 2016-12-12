a74e254e018fe1673c15b4fcc285ba37ec204425
philippe-thomas <philippe.thomas@dfki.de>
2016-12-12 17:01:58 +0100

    first commit

---
+++ src/main/java/de/dfki/nlp/SethTipsApplication.java
@@ -0,0 +1,12 @@
+package de.dfki.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethTipsApplication.class, args);
+	}
+}
---
+++ src/test/java/de/dfki/nlp/SethTipsApplicationTests.java
@@ -0,0 +1,16 @@
+package de.dfki.nlp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SethTipsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
