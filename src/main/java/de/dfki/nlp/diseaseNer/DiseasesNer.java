2eb279d5a82218a0d95e27a2640e3d2c51e024b4
Johannes <jkirschnick@gmail.com>
2017-01-31 16:39:55 +0100

    Correct parsing

--- src/main/java/de/dfki/nlp/diseaseNer/DiseasesNer.java
+++ src/main/java/de/dfki/nlp/diseaseNer/DiseasesNer.java
@@ -43,7 +43,7 @@ public class DiseasesNer {
                         allNames.add(line.toLowerCase());
                     }
 
-                    return false;
+                    return true;
                 }
 
                 @Override
